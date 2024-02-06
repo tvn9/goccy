f9a94d1c4648f962e4cad19b782a768b38a11444
Thanh Nguyen <tv.nguyen9@outlook.com>
2024-02-05 21:33:49 -0800

    Adding mutex, and dining example

--- dining/main.go
+++ dining/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 // The dining philosophers problems
 
@@ -29,5 +33,66 @@ var (
 )
 
 func main() {
+	// print out a welcome message
+	fmt.Println("Dinning Philosophers Problem")
+	fmt.Println("----------------------------")
+	fmt.Println("The table is empty.")
 
+	// start the meal
+	dine()
+
+	// print out finished message
+	fmt.Println("The table is empty.")
+}
+
+func dine() {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks.
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal.
+	for i := 0; i < len(philosophers); i++ {
+		// fire off a goroutine for the current philosophers
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+}
+
+func diningProblem(phi Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seat *sync.WaitGroup) {
+	defer wg.Done()
+
+	// seat the philosopher at the table
+	fmt.Printf("%s is seated at the table.\n", phi.name)
+	seat.Done()
+
+	seat.Wait()
+
+	// eat three times
+	for i := hunger; i > 0; i-- {
+		// get a lock on both forks
+		forks[phi.leftFork].Lock()
+		fmt.Printf("\t%s takes the left fork.\n", phi.name)
+		forks[phi.rightFork].Lock()
+		fmt.Printf("\t%s takes the right fork.\n", phi.name)
+
+		fmt.Printf("\t%s hask both forks and is eating.\n", phi.name)
+		time.Sleep(eatTime)
+
+		fmt.Printf("\t%s philosopher is thinking.\n", phi.name)
+		time.Sleep(thinkTime)
+
+		forks[phi.leftFork].Unlock()
+		forks[phi.rightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", phi.name)
+	}
+	fmt.Println(phi.name, "is satisfied.")
+	fmt.Println(phi.name, "left the table.")
 }
